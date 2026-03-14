11591fbe0ac775ebdf429d7e9d7a409978a973ec
DashDev <cutesundar4u@gmail.com>
2026-03-14 19:05:04 +0800

    Initial commit

--- Lesson 1 Activty 1/main.py
+++ Lesson 1 Activty 1/main.py
@@ -1,3 +1,4 @@
+from time import sleep
 print("Hello! I am an AI bot, what's your name? : ")
 
 name = input()
@@ -14,4 +15,16 @@ elif mood == "bad":
 else:
     print("I see. Sometimes, it's hard to put feelings into words.")
 
+question = input("What other language do you speak? ")
+print(f"Oh nice! You speak {question}!")
+
+question2 = input("I have one more question, do you like programming? yes/no: ")
+if question2 == "Yes" or question2 == "Yes".lower():
+    print("That's great to hear! Programming is a really fun thing to do.")
+elif question2 == "No":
+    print("It's okay, but you should try programming, it's fun! :)")
+else:
+    print("Sorry, I didn't get you..")
+
+sleep(1)
 print(f"It was nice chatting with you {name}. Goodbye! :D")

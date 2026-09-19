7e4a489e4c94e036133f6c1f7e66a9d87ea219a0
DashDev <cutesundar4u@gmail.com>
2026-09-19 19:27:16 +0800

    Initial commit

--- Lesson19 - NewMod/main.py
+++ Lesson19 - NewMod/main.py
@@ -5,13 +5,23 @@ url = "https://official-joke-api.appspot.com/random_joke"
 running = True
 # send GET req
 
-response = requests.get(url)
+while running:
+    response = requests.get(url)
 
-if (response.status_code == 200):
-    joke_data = response.json()
-    print(f"Joke: {joke_data['setup']} - {joke_data['punchline']}")
-else:
-    print("FAILED TO RETRIEVE JOKE.")
+    if (response.status_code == 200):
+        joke_data = response.json()
+        print(f"Joke: {joke_data['setup']} - {joke_data['punchline']}")
+        print(response.json())
+    else:
+        print("FAILED TO RETRIEVE JOKE.")
+
+    a = input("Do you want another joke? (y/n)")
+    if (a != "y"):
+        break
+    
+
+
+
 
     
 
